2c680271db9ad5bc3ff67ab57b9409d2614e8e55
Akuming <akwasiakumingamoah@gmail.com>
2022-09-19 05:05:28 +0000

    Initial Commit

---
+++ src/LandingPage.js
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return (
+        <div className="landingPage py-11 my-11">
+            <h1>Riddles For Everyone</h1>
+            <p>Welcome to Round4. Here you will find riddles with a twist. All riddles have at least 3 clues.</p>
+            <p>Gain fame and have fun by solving each clue before the riddle becomes ridiculously easy!!!</p>
+        </div> 
+     );
+}
+ 
+export default LandingPage;
